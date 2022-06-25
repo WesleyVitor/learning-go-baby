6ae79483d3e3b440ec7711300715f9bbf161d0d4
WesleyVitor <wesleydemorais@outlook.com.br>
2022-06-25 11:47:35 -0300

    add comments in test hello

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -5,7 +5,8 @@ import "testing"
 func TestHello(t *testing.T){
 
 	assertCorrectMessage := func(t testing.TB,got string, want string){
-		t.Helper()
+		/*Helper Function to DRY of tests */
+		t.Helper() // Allow to know what line was broke in realy test
 		if got != want{
 			t.Errorf("got %q want %q",got,want)
 		}
